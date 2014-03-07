52412bed62effe182603297b981dedd5a09543c9
marcwright <marc.wright@gmail.com>
2014-03-07 10:24:27 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foobartender::Application.config.secret_key_base = '1a58b6c2f9ed68670785101dbe74e04e6e14e82d5861ed79adb387c850ac479f06a9f7fa0fe8874b2cddc293d3dbb25a3bc7a2baa6cfe98d4e1c4e1d5ab52d44'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foobartender::Application.config.session_store :cookie_store, key: '_foobartender_session'
